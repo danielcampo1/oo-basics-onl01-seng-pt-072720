c8fe55071c4aff41f5520dafa7bfcc108de39a6e
daniel campo <campo_daniel94@yahoo.com>
2020-08-15 04:32:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,12 @@
 # spec/02_shoe_spec.rb
 
 class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
 end
